267c8dc6db4fe3bb7a349650b29487e3db5a569e
Jeremiasz Walczak <jeremiasz.walczak@gmail.com>
2018-07-14 13:51:22 +0200

    Signed-off-by: Jeremiasz Walczak <jeremiasz.walczak@gmail.com>

--- app.js
+++ app.js
@@ -1,52 +1,29 @@
-
-
-
-function setMainBusiness() {
-
-	document.getElementById("main-personal").style.display = "none";
-	document.getElementById("main-agency").style.display = "none";
-
-	document.getElementById("main-business").style.display = "block";
-
-	document.getElementById("bg-change").classList.add('business-bg');
-	document.getElementById("bg-change").classList.remove('personal-bg');
-	document.getElementById("bg-change").classList.remove('agency-bg');
-
-
-}
-
-
-function setMainPersonal() {
-
-	document.getElementById("main-agency").style.display = "none";
-	document.getElementById("main-business").style.display = "none";
-
-	document.getElementById("main-personal").style.display = "block";
-
-	document.getElementById("bg-change").classList.add('personal-bg');
-	document.getElementById("bg-change").classList.remove('agency-bg');
-	document.getElementById("bg-change").classList.remove('business-bg');
-
-}
-
-
-function setMainAgency() {
-
-	document.getElementById("main-personal").style.display = "none";
-	document.getElementById("main-business").style.display = "none";
-
-	document.getElementById("main-agency").style.display = "block";
-
-	document.getElementById("bg-change").classList.remove('personal-bg');
-	document.getElementById("bg-change").classList.add('agency-bg');
-	document.getElementById("bg-change").classList.remove('business-bg');
-
-}
-
 let slider = document.getElementById("mySlider");
 let output = document.getElementById("slider-value");
 
 output.innerHTML = slider.value; 
 slider.oninput = function() {
     output.innerHTML = this.value;
+}
+
+
+function setMain(businessplan) {
+
+	let main_element = "main-" + businessplan;
+	let bg_element = businessplan + "-bg";
+
+//background + plan change
+	$('#bg-change').removeClass().addClass(bg_element);
+	$('#main-business').css("display", "none")
+	$('#main-agency').css("display", "none")
+	$('#main-personal').css("display", "none")
+	$('#'+ main_element).css("display", "block");
+//memo change	
+	if (main_element == "main-business") {
+		document.getElementById("memo-change").innerHTML = "We offer plans for Businesses of all sizes.";
+	} else if (main_element == "main-agency") {
+		document.getElementById("memo-change").innerHTML = "For Agencies and Consultants working with client sites.";
+	} else if (main_element == "main-personal") {
+		document.getElementById("memo-change").innerHTML = "For personal projects and sites with low traffic.";
+	} 
 }
