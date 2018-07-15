030c1d215162309e00c30b396f10d395fd943420
Jeremiasz Walczak <jeremiasz.walczak@gmail.com>
2018-07-15 13:59:09 +0200

    Signed-off-by: Jeremiasz Walczak <jeremiasz.walczak@gmail.com>

--- app.js
+++ app.js
@@ -9,15 +9,50 @@ function requestJson(theUrl) {
     return JSON.parse(request.responseText);
 }
 
+// shows plans over 400k and hides button
+function setMorePlans() {
+	$('.more-plans').css("display", "block");
+	$('.higher-plans').css("display", "none");
+}
+
+//change buttons from red to outlined
+function changePlanButtons() {
+	$('.changable-button').removeClass("btn-danger").addClass("btn-outline-secondary");
+}
+
+
+function colorPlanButtons(rowNumber) {
+	let row_number = rowNumber
+	$('.underlined-rows .row').css("background-color", "white");
+	changePlanButtons();
+	$('.'+row_number).css("background-color", "lightgrey");
+	$('.'+row_number+ ' .changable-button').removeClass("btn-outline-secondary").addClass("btn-danger");
+}
+
+
 let sliderData = requestJson("sliderdata.json");
-//slider is now json key/value pair
+
+//slider is now json key/value pair that gets displayed
 output.innerHTML = sliderData[slider.value]; 
 slider.oninput = function() {
     output.innerHTML = sliderData[this.value];
     if (output.innerHTML == sliderData["81"]) {
     	output.innerHTML +="+" // slider displaying 2000000+
     }    
+    
+    if (this.value != 13) { changePlanButtons(); }
+    if (this.value > 60) { setMorePlans(); }
+
+    if (this.value >= 68 ) { colorPlanButtons("row2000k");}
+    if (this.value < 68) { colorPlanButtons("row800k"); }
+    if (this.value < 61) { colorPlanButtons("row400k"); }
+    if (this.value < 34) { colorPlanButtons("row120k"); }
+    if (this.value < 20) { colorPlanButtons("row50k"); }
+    if (this.value < 15) { colorPlanButtons("row20k"); }
+    if (this.value < 6) { colorPlanButtons(); }
 }
+
+
 
 
 
@@ -28,9 +63,9 @@ function setMain(businessplan) {
 
 //background + plan change
 	$('#bg-change').removeClass().addClass(bg_element);
-	$('#main-business').css("display", "none")
-	$('#main-agency').css("display", "none")
-	$('#main-personal').css("display", "none")
+	$('#main-business').css("display", "none");
+	$('#main-agency').css("display", "none");
+	$('#main-personal').css("display", "none");
 	$('#'+ main_element).css("display", "block");
 //memo change	
 	if (main_element == "main-business") {
@@ -41,5 +76,4 @@ function setMain(businessplan) {
 		document.getElementById("memo-change").innerHTML = "For personal projects and sites with low traffic.";
 	} 
 }
-
 
